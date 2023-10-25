90f440972d5bbd23f281d50bcb6fb877dd087d53
abrodskiy <abrodskiy@indeed.com>
2023-10-25 14:34:21 -0400

    Placeholder navbar replaced with breadcrumb navbar for CreateCard component

--- src/Layout/features/card/CreateCard.js
+++ src/Layout/features/card/CreateCard.js
@@ -37,7 +37,13 @@ function CreateCard() {
     }
     return (
         <div>
-            <h2>Breadcrumb Navbar <Link to='/'>Home</Link></h2>
+            <nav aria-label="breadcrumb">
+                <ol className="breadcrumb">
+                    <li className="breadcrumb-item"><Link to='/'>Home</Link></li>
+                    <li className="breadcrumb-item"><Link to={`/decks/${deckId}`}>{deck.name}</Link></li>
+                    <li className="breadcrumb-item active" aria-current="page">Add Card</li>
+                </ol>
+            </nav>
             <h3>{deck.name}: Add Card</h3>
             <CardForm card={emptyCard} dbSubmit={addCard}/>
         </div>
