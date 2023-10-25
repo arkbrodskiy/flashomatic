09fa1b08c9548eb738ce56989a66688ea7e28257
abrodskiy <abrodskiy@indeed.com>
2023-10-25 16:11:51 -0400

    font awesome icon added to NotEnoughCards component

--- src/Layout/NotEnoughCards.js
+++ src/Layout/NotEnoughCards.js
@@ -1,12 +1,19 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPlus } from '@fortawesome/free-solid-svg-icons'
 
 function NotEnoughCards({ deck }) {
     return (
         <div className="NotFound">
             <h2>Not enough Cards</h2>
             <p>You need at least 3 cards to study. There are {deck.cards.length} cards in this deck</p>
-            <Link to={`/decks/${deck.id}/cards/new`} className="btn btn-primary">Add Cards</Link>
+            <Link
+                to={`/decks/${deck.id}/cards/new`}
+                className="btn btn-primary"
+            >
+                {<FontAwesomeIcon icon={faPlus} />} Add Cards
+            </Link>
         </div>
     );
 }
