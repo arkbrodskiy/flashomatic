d491acefeb20a2fe5c69ca4158900800ae8f5857
abrodskiy <abrodskiy@indeed.com>
2023-10-24 21:27:59 -0400

    Delete Card functionality added

--- src/Layout/features/card/CardFullView.js
+++ src/Layout/features/card/CardFullView.js
@@ -1,7 +1,25 @@
-import React from "react";
-import { Link, useRouteMatch } from "react-router-dom";
+import React, {useEffect, useState} from "react";
+import { Link, useRouteMatch, useHistory } from "react-router-dom";
+import { deleteCard } from "../../../utils/api";
 
 function CardFullView({ card }) {
+    const history = useHistory()
+    const [deletePressed, setDeletePressed] = useState(false)
+    useEffect( () => {
+        const abortController = new AbortController()
+        const removeCard = async () => {
+            try {
+                if (window.confirm("Delete this card? You will not be able to recover it")) {
+                    await deleteCard(card.id, abortController.signal)
+                }
+                history.go(0)
+            } catch (err) {
+                if (err.name !== 'AbortError') throw err
+            }
+        }
+        if (deletePressed) removeCard()
+        return () => abortController.abort()
+    }, [deletePressed])
     const {url} = useRouteMatch()
     return (
         <div>
@@ -11,7 +29,7 @@ function CardFullView({ card }) {
                         <p className="card-text">{card.front}</p>
                         <p className="card-text">{card.back}</p>
                         <Link to={`${url}/cards/${card.id}/edit`} className="btn btn-secondary">Edit</Link>
-                        <a href="#" className="btn btn-danger">Delete</a>
+                        <button onClick={() => setDeletePressed(true)} className="btn btn-danger">Delete</button>
                     </div>
                 </div>
             </div>
